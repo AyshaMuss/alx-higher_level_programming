b51ae0ecbf915da6f4e492b8fd0caf48ec6ca1ae
AyshaMuss <Ayshamuss@gmail.com>
2021-11-14 19:44:21 +0300

    python hello world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a linked list has a cycle
+ * @list: Pointer to head
+ * Return: 1 if it has a cycle, 0 if it doesn't
+**/
+int check_cycle(listint_t *list)
+{
+listint_t *one, *two;
+one = list;
+two = list;
+
+while (two != NULL && two->next != NULL)
+{
+one = one->next;
+two = two->next->next;
+if (one == two)
+return (1);
+}
+return (0);
+}
