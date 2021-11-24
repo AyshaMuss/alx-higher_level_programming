6f84dbff5f9fe467b48b35dbf5816c3c3ff49578
AyshaMuss <Ayshamuss@gmail.com>
2021-11-24 17:18:15 +0300

    0x04 python more data structures

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):  
-    total = my_list
-    for x in numbers:
-        total *= x  
-    return total 
+    return list(map(x).__mul__, my_list))
